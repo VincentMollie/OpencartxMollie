90649256cf0c89e9972ef794dc4c6b07fc95f463
Salim Bouziane Rabhi <salim.bouziane.rabhi@mollie.com>
2023-04-06 13:45:12 +0200

    Currency fix

--- .docker/shell.sh
+++ .docker/shell.sh
@@ -1,7 +1,16 @@
-#!/bin/bash
+#! /bin/bash
+
+echo "[!] Waiting for mariadb to start!"
+
+sleep 15
+
+echo "[!] Doing stuff!"
+
 mysql -u bn_opencart -h mariadb << EOF
 USE bitnami_opencart;
 SELECT * FROM oc_currency;
 UPDATE oc_currency SET status=0 WHERE currency_id=1;
 UPDATE oc_currency SET status=0 WHERE currency_id=2;
 EOF
+
+echo "[!] Completed stuff!"
